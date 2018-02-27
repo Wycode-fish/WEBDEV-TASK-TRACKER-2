74fb4375bc5ac939f957832b78f5de254d29e0b9
eason <164335424@qq.com>
2018-02-27 01:03:46 +0000

    remove task_tracker directory

--- task_tracker/priv/repo/migrations/20180222224816_add_fields_to_users.exs
+++
@@ -1,9 +0,0 @@
-defmodule TaskTracker.Repo.Migrations.AddFieldsToUsers do
-  use Ecto.Migration
-
-  def change do
-    alter table(:users) do
-      add :manager_id, references(:users, on_delete: :delete_all)
-    end
-  end
-end
